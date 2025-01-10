eb07a8cd3374b220b002373673924a12414f64ce
Eugene <pshonovskij@gmail.com>
2025-01-10 22:40:53 +0200

    Cleaned some files

--- src/ContactManager/ContractManager.Communication/Dtos/Endpoints/Get/GetResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ContractManager.Communication.Dtos.Endpoints.Get
-{
-    public class GetResponse
-    {
-        public IEnumerable<ContactResponse> Data { get; set; } = Enumerable.Empty<ContactResponse>();
-        public int TotalCount { get; set; }
-    }
-}
